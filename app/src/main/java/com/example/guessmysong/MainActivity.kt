b9ba2bb6108081c1420c63b8044726cdba2de51a
Andrés Tirado <andres.tirado@netlight.com>
2020-12-06 16:36:16 +0100

    Testing something in Github

--- app/src/main/java/com/example/guessmysong/MainActivity.kt
+++ app/src/main/java/com/example/guessmysong/MainActivity.kt
@@ -2,9 +2,9 @@ package com.example.guessmysong
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
             GuessMySongTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Hej Android")
                 }
             }
         }
